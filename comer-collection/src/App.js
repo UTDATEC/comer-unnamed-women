fdb35f92b20627ea42d6dcb6b6388500cfccd2d6
JordanTammUTD <jat180001@utdallas.edu>
2022-03-16 11:59:49 -0500

    Jordan Tamm's Branch
    First changes

--- comer-collection/src/App.js
+++ comer-collection/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload test.
+          Edit <code>src/App.js</code> and save to reload test Jordan.
         </p>
         <a
           className="App-link"
