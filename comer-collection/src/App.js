52fe7bd7555b1b6d8925460df33677dab31abd62
JordanTammUTD <jat180001@utdallas.edu>
2022-03-20 20:04:56 -0500

    Header
    Header Creation

--- comer-collection/src/App.js
+++ comer-collection/src/App.js
@@ -4,19 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
         <p>
-          Edit <code>src/App.js</code> and save to reload test Jordan.
+         My Test Text
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </header>
+      <header className="App-header">
       </header>
     </div>
   );
