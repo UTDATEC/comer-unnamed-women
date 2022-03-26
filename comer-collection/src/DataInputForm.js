67410c1751fbf9a0f0cdda12bd85541a06bf7d0d
JordanTammUTD <jat180001@utdallas.edu>
2022-03-26 18:31:11 -0500

    Ingestion Engine Page UI
    Header, form in grid with REQUIRED text boxes, submit button

---
+++ comer-collection/src/DataInputForm.js
@@ -0,0 +1,104 @@
+// const DataInputForm = () => {
+//     return (
+//         <div className="dataInput">
+//             <p>
+//             Data Input Form
+//              </p>
+//         </div> 
+//     );
+// }
+
+import './DataInputForm.css';
+import Grid from '@material-ui/core/Grid'
+
+const DataInputForm = () => {
+    return (
+        <div className="dataInputForm">
+      <h2>Ingestion Engine Input</h2>
+      <form>
+        <Grid container spacing={3}>
+        <Grid item xs={5} sm={5}>
+        <label>Title</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Artist</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Tags</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Inscriptions</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Date</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Medium</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Dimensions</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Accession Number</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Copyright</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Subject</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        <Grid item xs={5} sm={5}>
+        <label>Collection Location</label>
+        <input 
+          type="text" 
+          required 
+        />
+        </Grid>
+        </Grid>
+        <button>Submit</button>
+      </form>
+    </div>
+    );
+}
+
+export default DataInputForm;
