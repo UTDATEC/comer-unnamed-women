8fd00b16037807cf3fc0158a0a7ba2358644f444
sarahnhasan <60158618+sarahnhasan@users.noreply.github.com>
2022-03-29 18:42:51 -0500

    Textbox
    Implemented testing textbox. I tried.

--- comer-collection/src/App.js
+++ comer-collection/src/App.js
@@ -4,22 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="fname">Textbox</label>
+      <div>
+      <input type="text" id="fname" name="fname"></input>
+      </div>
+     
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
