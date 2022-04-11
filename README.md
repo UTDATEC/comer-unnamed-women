2b80551089f695eac61fb5a36cae35eacbe5e27a
jithinlucas <jithinlucas@gmail.com>
2022-04-11 07:54:06 -0500

    Search By Page
    frontend for page created

--- README.md
+++
@@ -1,2 +0,0 @@
-# comer-unnamed-women
-Project space for Spring 2022 UTDesign project for UTD's Comer Collection and xtine burrough's AN Archive of Unnamed Women
--- comer-collection/src/App.js
+++ comer-collection/src/App.js
@@ -1,21 +1,141 @@
-import logo from './logo.svg';
 import './App.css';
+import { View } from 'react-native';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const btn = document.getElementById('btn');
+
+const useStyles = makeStyles((theme) => ({
+  abRoot: {
+  backgroundColor: '#e87500',
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  input1: {
+    height: 50
+  },
+}));
 
 function errorMessage()
 {
-  alert('Error')
-  //btn.style.backgroundColor = 'salmon';
-  //btn.style.color = 'white';
+  alert('Searching...')
 }
 
 function App() {
+  const classes = useStyles();
   return (
+    
+
     <div className="App">
+      <AppBar position="fixed" classes={{ root: classes.abRoot}}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color= "inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Search By Page
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+
+      
+      <a class="align1">
+      <text>
+      <br /><br /><br /><br /><br /><br />Title<br />
+      </text>
+      <TextField id="filled-basic" label="Title" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      <text>
+      <br /><br />Artist<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Artist" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br /><br />Tags<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Tags" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br /><br />Inscriptions<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Inscriptions" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br /><br />Date Created<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Date Created" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      </a>
+      <a class="align2">
+      <text>
+      <br /><br /><br /><br /><br /><br />Medium<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Medium" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br />Dimensions<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Dimensions" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br /><br />Accession Number<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Accession Number" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br /><br />Copyright<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Copyright" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      <text>
+      <br /><br />Subject<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Subject" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      </a>
+      <a className = "align3">
+      <text>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />Collection Location<br />
+      </text>
+      <div>
+      <TextField id="filled-basic" label="Collection Location" variant="filled" margin="normal" style = {{width: 500}} InputProps={{ classes: { input: classes.input1 } }}/>
+      </div>
+      </a>
+      <a className = "align4">
+      <text>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      </text>
       <button class = "myButton" onClick = {errorMessage}>
-        Button
+        Search
       </button>
+      <text>
+        <br /><br /><br /><br /><br /><br />
+      </text>
+      </a>
+
     </div>
   );
 }
