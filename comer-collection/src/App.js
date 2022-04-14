e9de5eae221a356976d58a6436c27d943a492a34
JordanTammUTD <jat180001@utdallas.edu>
2022-04-14 16:36:13 -0500

    css clean

--- comer-collection/src/App.js
+++ comer-collection/src/App.js
@@ -2,15 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import DataInputForm from './DataInputForm';
 
-const btn = document.getElementById('btn');
-
-function submitMessage()
-{
-  alert('Submitted Successfully!')
-  //btn.style.backgroundColor = 'salmon';
-  //btn.style.color = 'white';
-}
-
 function App() {
   return (
     <div className="App">
