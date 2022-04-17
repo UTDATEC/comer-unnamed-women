6cbec7d60b70e92b322e2117208de3c49ba0a490
JordanTammUTD <jat180001@utdallas.edu>
2022-04-17 13:00:40 -0500

    Working Mock Post request

--- comer-collection/src/server.js
+++ comer-collection/src/server.js
@@ -10,6 +10,25 @@ export function makeServer() {
             { id: 3, text: "Work out" },
           ],
       }))
+
+      this.post("/api/post", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody)
+        console.log(attrs)
+      })
+
+      // this.post("/api/betterpost?", function (schema, request) {
+      //   let attrs = JSON.parse(request.requestBody).author
+  
+      //   if (attrs.name) {
+      //     return schema.authors.create(attrs)
+      //   } else {
+      //     return new Response(
+      //       400,
+      //       { some: "header" },
+      //       { errors: ["name cannot be blank"] }
+      //     )
+      //   }
+      // })
     },
   })
 }
