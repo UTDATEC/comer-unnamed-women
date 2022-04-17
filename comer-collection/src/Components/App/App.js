b66d1039823da8c2636ab64ae8832c17cfeb7a09
sbj00 <sbj170000@utdallas.edu>
2022-04-17 17:45:27 -0500

    Added Sarah's Code

--- comer-collection/src/Components/App/App.js
+++ comer-collection/src/Components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import SearchPage from '../SearchPage/SearchPage'
+import SearchPage2 from '../SearchPage2/SearchPage2'
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <h1>Marine Mammals</h1>
       <BrowserRouter>
         <Switch>
+          <Route path="/searchpage">
+            <SearchPage2/>
+          </Route>
           <Route path="/">
             <SearchPage/>
           </Route>
