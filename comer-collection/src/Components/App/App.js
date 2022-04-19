dbf317965ca18915ff9d2d4be25138519c4648ff
sbj00 <sbj170000@utdallas.edu>
2022-04-19 11:27:35 -0500

    Added Tamms Page

--- comer-collection/src/Components/App/App.js
+++ comer-collection/src/Components/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SearchPage from '../SearchPage/SearchPage'
 import SearchPage2 from '../SearchPage2/SearchPage2'
 import SearchBy from '../SearchBy/SearchBy'
+import DataInput from '../DataInput/DataInputForm'
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route path="/searchpage2">
             <SearchPage2/>
+          </Route>
+          <Route path="/DataInputForm">
+            <DataInput/>
           </Route>
           <Route path="/">
             <SearchBy/>
