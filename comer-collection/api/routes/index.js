6d354c8db166936c163cbb1cb60af2abba238481
JordanTammUTD <jat180001@utdallas.edu>
2022-04-23 11:44:41 -0500

    working post method

--- comer-collection/api/routes/index.js
+++ comer-collection/api/routes/index.js
@@ -1,9 +1,65 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const db = require("../sequelize.js");
+const Tutorial = db.tutorials;
+//const Op = db.Sequelize.Op;
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize({
+//     HOST: "127.0.0.1",
+//     USER: "root",
+//     PASSWORD: "monktamm123",
+//     DB: "Comer",
+//     dialect: "mysql",
+//     operatorsAliases: false,
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+// router.get("/", function(req, res, next) {
+//     res.send("API WORKING");
+// });
+
+router.post('/post', function(req, res, next) {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Tutorial
+  const tutorial = {
+    title: req.body.title,
+    description: req.body.artist,
+    published: true
+  };
+
+  // Save Tutorial in the database
+  Tutorial.create(tutorial)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
 });
+
+  // this.post("/api/post", (schema, request) => {
+  //   let attrs = JSON.parse(request.requestBody)
+  //   console.log(attrs)
+  // })
 
 module.exports = router;
