c4042401f700bde02ead32f53f21174198b17081
JordanTammUTD <jat180001@utdallas.edu>
2022-05-04 15:05:19 -0500

    Finished code!

--- comer-collection/src/Components/SearchPage2/SearchPage2.js
+++ comer-collection/src/Components/SearchPage2/SearchPage2.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-//import logo from './logo.png';
 import './SearchPage2.css';
-//import IconButton from "@material-ui/core/IconButton";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import SearchIcon from "@material-ui/icons/Search";
-import TextField from '@material-ui/core/TextField';
-//.Logo {
- // background-image: url(./logo.png);
-//}
-//import {  TextField, IconButton } from '@material-ui/core';
-import { SearchOutlined } from '@material-ui/icons';
-import { ReactComponent as Logo } from './utd.svg';
-import { ReactComponent as DashboardIcon } from './utd.svg';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const useStyles = makeStyles((theme) => ({
   abRoot: {
@@ -36,16 +22,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const btn = document.getElementById('btn');
-
-function errorMessage()
-{
-  alert('Error')
-}
-
-
-
-
 function SearchPage2(props) {
   const classes = useStyles();
   const history = useHistory();
@@ -61,29 +37,39 @@ function SearchPage2(props) {
       </Toolbar>
     </AppBar> 
 
-    <div className="align-center">
-      <img className="img" src={`http://localhost:9000/images/${props.selectedImage.fileName}`} />
-      <div className = "details">
-        Title: {props.selectedImage.title}
-        Artist: {props.selectedImage.artist}
-        Medium: {props.selectedImage.medium}
-        Date: {props.selectedImage.dateCreated}
-        Collection Location: {props.selectedImage.collectionLocation}
-        Tags: {props.selectedImage.tags}
-        Inscriptions: {props.selectedImage.inscriptions}
-        Copyrights Holder: {props.selectedImage.copyright}
+    <Container>
+    <Col>
+      <Row>
+      <div className="align-center">
+        <img className="img" src={`http://localhost:9000/images/${props.selectedImage.fileName}`} />
       </div>
-    </div>
+      </Row>
+      <Row>
+        <div className = "demo">
+          <span><b> Title: </b> <i>{props.selectedImage.title}</i> </span>
+          <span><b> Artist: </b> <i>{props.selectedImage.artist}</i> </span>
+          <span><b> Medium: </b> <i>{props.selectedImage.medium}</i> </span>
+        </div>
+      </Row>
+      <Row>
+      <div className = "demo">
+          <span><b> Tags: </b> <i>{props.selectedImage.tags}</i> </span>
+          <span><b> Inscriptions: </b> <i>{props.selectedImage.inscriptions}</i> </span>
+          <span><b> Copyrights Holder: </b> <i>{props.selectedImage.copyright}</i> </span>
+        </div>
+      </Row>
+    </Col>
+    </Container>
 
 
 
 
     <div class="align-right">
-      <button class = "myButton" onClick = {errorMessage} style={{width:300}}>
+      <button class = "myButton" style={{width:300}}>
         ADD TO AN EXHIBITION
       </button>
 
-      <button class = "myButton" onClick = {errorMessage} style={{width:300}}>
+      <button class = "myButton"  style={{width:300}}>
         EDIT TAGS
       </button>
     </div>
