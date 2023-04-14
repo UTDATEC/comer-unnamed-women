25b022a30f37b084ce675304684256f4607c9e98
HarrisonHoo <hah180003@utdallas.edu>
2023-04-13 20:14:17 -0500

    Merge branch 'Harrison' into front-end-SP23

--- comer-collection/src/Components/GridView/GridView.js
+++ comer-collection/src/Components/GridView/GridView.js
@@ -1,8 +1,6 @@
 import React, { Component, useEffect, useState } from 'react';
 // import logo from './utd.svg';
 import '../App/App.css';
-import ButtonAppBar from './ButtonAppBar'
-import Cards from './Cards'
 import Images from './Images'
 import NavBar from "../NavBar/NavBar";
 
