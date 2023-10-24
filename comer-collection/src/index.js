98111104adba9c8d1b6feb2eb9da8e69fe09e01b
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-10-24 16:01:47 -0500

    Remove unnecessary files

--- comer-collection/src/index.js
+++ comer-collection/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
-//import { makeServer } from './server';
 
 /*if (process.env.NODE_ENV === 'development') {
   console.log("making server");
