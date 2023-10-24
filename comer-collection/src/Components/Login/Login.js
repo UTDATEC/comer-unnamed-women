356a0e1778dcb5be6918a015b52cc982cff3155f
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-10-24 15:38:36 -0500

    Fix Login.js

--- comer-collection/src/Components/Login/Login.js
+++ comer-collection/src/Components/Login/Login.js
@@ -1,18 +1,36 @@
+import { Component } from "react";
+import { withRouter } from "react-router-dom/cjs/react-router-dom";
+
+import "./LoginForm.css";
+
 class Login extends Component {
     push = () => {
         this.props.history.push("/login");
     }
 
+    
+    handleSubmit = () => {
+    // Perform any submission logic here, e.g., sending the email list to a server.
+    // You can access the email list in this.state.emailList.
+    };
+
+    
+    handleChange = () => {
+    // Perform any submission logic here, e.g., sending the email list to a server.
+    // You can access the email list in this.state.emailList.
+    };
+
     render () {
+         // TODO: Fix value field
         return (
             <div className="loginForm">
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <label>Email</label>
                     <input
                         type="text"
                         name="email"
-                        value={form.email}
-                        onchange={handleChange}
+                        value={undefined}
+                        onChange={this.handleChange}
                     />
                 </form>
             </div>
