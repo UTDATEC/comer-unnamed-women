111cce6da542a3f0ea0e2afad01903a9d039b7a8
djoh826 <119085651+djoh826@users.noreply.github.com>
2023-10-26 22:39:05 -0500

    Login Page + Login Button

---
+++ comer-collection/src/Components/Login/Login.js
@@ -0,0 +1,48 @@
+import './Login.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+import { Row, Col, Container } from 'react-bootstrap';
+import NavBar from '../NavBar/NavBar';
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+// import '../App/App.css';
+// import '../SearchBy/DataInputForm.css';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div class="separator" />
+        <div class="loginForm">
+          <form>
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              // value={this.state.value}
+              // onChange={this.handleChange}
+              // required
+            />
+
+            <label>Password</label>
+            <input
+              type="text"
+              name="password"
+              // value={this.state.value}
+              // onChange={this.handleChange}
+              // required
+            />
+
+            <button id="centered">Login</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
--- comer-collection/src/Components/NavBar/NavBar.js
+++ comer-collection/src/Components/NavBar/NavBar.js
@@ -10,58 +10,72 @@ import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-    abRoot: {
-        backgroundColor: 'darkgreen',
-    },
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        flexGrow: 1,
-        textAlign: "right",
-    },
-    buttonText: {
-        color: "white",
-    },
-    title: {
-        flexGrow: 1,
-        textAlign: "center",
-        color: 'white',
-        textTransform: "capitalize",
-    },
-    titleButton: {
-        flexGrow: 1,
-        textAlign: "left",
-        width: "20%",
-    }
+  abRoot: {
+    backgroundColor: 'darkgreen',
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    flexGrow: 1,
+    textAlign: 'right',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'capitalize',
+  },
+  titleButton: {
+    flexGrow: 1,
+    textAlign: 'left',
+    width: '20%',
+  },
 }));
 
 export default function NavBar() {
-    const classes = useStyles();
-    const history = useHistory();
+  const classes = useStyles();
+  const history = useHistory();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="fixed" classes={{ root: classes.abRoot}}>
-                <Toolbar>
-                    <Button styles={classes.titleButton} onClick={() => history.push('/')}>
-                    <Typography variant="h6" className={classes.title}>
-                        UTD Comer Collection
-                    </Typography></Button>
-                    <div className={classes.menuButton}>
-                        <Button className="myButton" onClick={() => history.push('/')}>
-                            <div className={classes.buttonText}>Images</div></Button>
-                        {/*<Button className="myButton" onClick={() => history.push('/expandedView')}>
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" classes={{ root: classes.abRoot }}>
+        <Toolbar>
+          <Button
+            styles={classes.titleButton}
+            onClick={() => history.push('/')}>
+            <Typography variant="h6" className={classes.title}>
+              UTD Comer Collection
+            </Typography>
+          </Button>
+          <div className={classes.menuButton}>
+            <Button className="myButton" onClick={() => history.push('/')}>
+              <div className={classes.buttonText}>Images</div>
+            </Button>
+            {/*<Button className="myButton" onClick={() => history.push('/expandedView')}>
                             <div className={classes.buttonText}>Expanded</div></Button>*/}
-                        &nbsp;&nbsp;
-                        <Button className="myButton" onClick={() => history.push('/exhibitMain')}>
-                            <div className={classes.buttonText}>Exhibitions</div></Button>
-                        &nbsp;&nbsp;
-                        <Button className="myButton" onClick={() => history.push('/searchBy')}>
-                            <div className={classes.buttonText}>Search</div></Button>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+            &nbsp;&nbsp;
+            <Button
+              className="myButton"
+              onClick={() => history.push('/exhibitMain')}>
+              <div className={classes.buttonText}>Exhibitions</div>
+            </Button>
+            &nbsp;&nbsp;
+            <Button
+              className="myButton"
+              onClick={() => history.push('/searchBy')}>
+              <div className={classes.buttonText}>Search</div>
+            </Button>
+            &nbsp;&nbsp;
+            <Button className="myButton" onClick={() => history.push('/login')}>
+              <div className={classes.buttonText}>Login</div>
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
