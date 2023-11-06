1474a5d24c21880bf567644fe9188a6cb99f2385
djoh826 <119085651+djoh826@users.noreply.github.com>
2023-11-06 15:52:12 -0600

    update

--- comer-collection/src/Components/Login/Login.js
+++ comer-collection/src/Components/Login/Login.js
@@ -1,4 +1,5 @@
 import './Login.css';
+import { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -13,30 +14,72 @@ import Grid from '@material-ui/core/Grid';
 // import '../App/App.css';
 // import '../SearchBy/DataInputForm.css';
 
-export default class Login extends Component {
+class Login extends Component {
+
+  // async function LoginUser()
+
+
+  //disable javascript.validate in vsc settings?
+  // [email, setEmail] = useState();
+  // [password, setPassword] = useState();
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+  
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleLogin(event) {
+    event.preventDefault();
+    //todo
+    alert(email);
+
+    // const response = await LoginUser({
+    //   email,
+    //   password
+    // });
+
+
+  }
+
   render() {
     return (
       <div>
         <NavBar />
         <div class="separator" />
         <div class="loginForm">
-          <form>
+          <form onSubmit={this.handleLogin}>
             <label>Email</label>
             <input
               type="text"
               name="email"
-              // value={this.state.value} (no functionality yet)
-              // onChange={this.handleChange}
-              // required
+              value={this.state.value}
+              onChange={this.handleChange}
+              required
             />
 
             <label>Password</label>
             <input style={{marginBottom: "11px"}}
               type="text"
               name="password"
-              // value={this.state.value}
-              // onChange={this.handleChange}
-              // required
+              value={this.state.value}
+              onChange={this.handleChange}
+              required
             />
             <button id="centered">Login</button>
           </form>
@@ -45,3 +88,5 @@ export default class Login extends Component {
     );
   }
 }
+
+export default withRouter(Login);
