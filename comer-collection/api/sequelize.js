3ebfc10e60b018f203fc134dcfde2286c32cb448
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-09 03:04:25 -0600

    Update sequelize.js

--- comer-collection/api/sequelize.js
+++ comer-collection/api/sequelize.js
@@ -1,37 +1,27 @@
 // Initialize sequelize
 const Sequelize = require("sequelize");
-const fs = require('node:fs');
 
-// Replace these (database name, username, password_) with whatever credentials and replace the host with the correct host
-/*const sequelize = new Sequelize('comerDb','root','MyNewPass', { 
-    dialect: 'mysql',
-    host:'localhost'
-
-});*/
 //import cCert from 'raw-loadeer!ls /Users/dwm160130/Library/Application\ Support/MySQL/Workbench/certificates/5B407BCB-CA91-49DD-8A10-9C2B437C6A75/client-key.pem';
 //import cKey from 'raw-loadeer!ls /Users/dwm160130/Library/Application\ Support/MySQL/Workbench/certificates/5B407BCB-CA91-49DD-8A10-9C2B437C6A75/client-key.pem';
 
-// Authentication object for querying from database
-
 const { DB_HOST, DB_PORT, DB_SCHEMA, DB_USER, DB_PASSWORD } = process.env;
 
+// Authentication object for querying from database
 const sequelize = new Sequelize(DB_SCHEMA,DB_USER, DB_PASSWORD, {
-        //const sequelize = new Sequelize('atc_sandbox','yourname','yourpassword', {
-        dialect: 'mysql',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-              
-            }
-          },
-        host: DB_HOST,
-        port: DB_PORT,
-        define: {
-            timestamps: false
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
         }
-}
-);
+    },
+    host: DB_HOST,
+    port: DB_PORT,
+    define: {
+        timestamps: false
+    }
+});
+
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
  }).catch((error) => {
