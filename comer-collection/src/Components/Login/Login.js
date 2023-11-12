5f1e8dac96179154f94579ded82fc58548ea5ce6
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-11 22:09:14 -0600

    Remove unnecessary imports

--- comer-collection/src/Components/Login/Login.js
+++ comer-collection/src/Components/Login/Login.js
@@ -1,18 +1,7 @@
 import './Login.css';
-import { useState } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import { useHistory } from 'react-router-dom';
-import { Row, Col, Container } from 'react-bootstrap';
 import NavBar from '../NavBar/NavBar';
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-// import '../App/App.css';
-// import '../SearchBy/DataInputForm.css';
 
 class Login extends Component {
   constructor(props) {
