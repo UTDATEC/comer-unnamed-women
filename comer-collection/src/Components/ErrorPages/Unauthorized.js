069348d5b512656e3ec3eaaa6cfdbbd0faa99920
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-17 03:43:44 -0600

    Add customization options to Unauthorized page

--- comer-collection/src/Components/ErrorPages/Unauthorized.js
+++ comer-collection/src/Components/ErrorPages/Unauthorized.js
@@ -1,13 +1,21 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import LockIcon from '@mui/icons-material/Lock'
+import { useNavigate } from 'react-router'
 
 const Unauthorized = (props) => {
 
+    const navigate = useNavigate();
+
+    const { message, buttonText, buttonDestination } = props;
+
     return (
-        <Box sx={{width: '100%', height: '100%', opacity: 0.5}}>
-            <Stack direction="column" alignItems="center" justifyContent="center" sx={{height: '100%'}}>
-                <LockIcon sx={{fontSize: '150pt'}} />
-                <Typography variant="h4">Unauthorized</Typography>
+        <Box sx={{width: '100%', height: '100%'}}>
+            <Stack direction="column" alignItems="center" justifyContent="center" spacing={2} sx={{height: '100%'}}>
+                <LockIcon sx={{fontSize: '150pt', opacity: 0.5}} />
+                <Typography variant="h4">{message ?? "Unauthorized"}</Typography>
+                <Button variant="contained" onClick={() => navigate(buttonDestination ?? '/')}>
+                    <Typography variant="body1">{buttonText ?? "Return to Homepage"}</Typography>
+                </Button>
             </Stack>
         </Box>
     )
