7345fdf70cbeb1480f17abb0b3f42962e5b38ebd
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-18 01:02:28 -0600

    Update UserManagement.js
    Expand table to full screen and add search functionality, as well as password pasting for temporary passwords

--- comer-collection/src/Components/Users/Admin/UserManagement.js
+++ comer-collection/src/Components/Users/Admin/UserManagement.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
-  IconButton,
   Paper,
   Stack,
   TableContainer, Dialog,
@@ -10,20 +9,26 @@ import {
   DialogActions,
   Button,
   Typography,
-  Switch
+  Switch,
+  Box,
+  TextField, InputAdornment, IconButton
 } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy"
 import Unauthorized from "../../ErrorPages/Unauthorized";
 
 const UserManagement = (props) => {
   const [curators, setCurators] = useState([]);
   const [deleteConfirmation, setDeleteConfirmation] = useState(false);
   const [curatorToDelete, setCuratorToDelete] = useState(null);
+
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { user, setUser, selectedNavItem, setSelectedNavItem } = props;
   
@@ -32,6 +37,7 @@ const UserManagement = (props) => {
     id: "ID",
     displayName: "Name",
     email: "Email",
+    password: "Password",
     courseCount: "Courses",
     exhibitionCount: "Exhibitions",
     type: "User Type",
@@ -117,31 +123,48 @@ const UserManagement = (props) => {
     <Unauthorized message="Insufficient Privileges" buttonText="Return to Profile" buttonDestination="/Account/Profile" />
   ) ||
   user.is_admin && (
-    <div style={{
-      marginLeft: '10%',
-      marginRight: '10%',
-      paddingTop: '5%'
-    }}>
-        <TableContainer component={Paper} sx={{ width: "100%" }}>
-          <Table size="small" aria-label="curator table" sx={{ width: "100%" }}>
-            <TableHead
-              sx={{
-                "& th": { fontWeight: "bold" },
-                "&": { backgroundColor: "lightgray" },
-              }}
-            >
-              <TableRow>
+    <>
+        <Stack direction="row" justifyContent="space-between" spacing={2} padding={2}>
+          <TextField variant="outlined" placeholder="Search" value={searchQuery} sx={{width: "50%"}}
+            onChange={(e) => {
+              setSearchQuery(e.target.value)
+            }}
+            InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}></TextField>
+          <Button variant="contained" disabled>
+            <AddIcon fontSize="large"/>
+            <Typography variant="body1" sx={{padding: '5px'}} >Create User</Typography>
+          </Button>
+        </Stack>
+        <TableContainer component={Paper} sx={{ width: "100%", maxHeight: 'calc(100% - 100px)' }}>
+          <Table stickyHeader size="small" aria-label="curator table" sx={{ width: "100%" }}>
+            <TableHead>
+              <TableRow sx={{backgroundColor: "#CCC"}}>
                 {Object.keys(curatorColumns).map((col) => (
-                  <TableCell key={col}>
+                  <TableCell key={col} sx={{backgroundColor: "#CCC"}}>
                     <Typography variant="h6">{curatorColumns[col]}</Typography>
                   </TableCell>
                 ))}
               </TableRow>
             </TableHead>
 
-            <TableBody sx={{ "& tr:hover": { backgroundColor: "#EEE" } }}>
+            <TableBody>
               {curators.map((curator) => (
-                  <TableRow key={curator.id}>
+                  <TableRow key={curator.id} hover={{backgroundColor: "#EEE"}} sx={{
+                    display: (
+                      searchQuery == "" ||
+                      Boolean(curator.family_name?.toLowerCase().includes(searchQuery.toLowerCase())) ||
+                      Boolean(curator.given_name?.toLowerCase().includes(searchQuery.toLowerCase())) ||
+                      Boolean(`${curator.given_name?.toLowerCase()} ${curator.family_name?.toLowerCase()}`.includes(searchQuery.toLowerCase())) ||
+                      Boolean(`${curator.family_name?.toLowerCase()}, ${curator.given_name?.toLowerCase()}`.includes(searchQuery.toLowerCase())) ||
+                      Boolean(curator.email?.replace("@utdallas.edu", "").toLowerCase().includes(searchQuery.toLowerCase()))
+                    ) ? "" : "none"
+                  }}>
                     <TableCell>
                       <Typography variant="body1">{curator.id}</Typography>
                     </TableCell>
@@ -150,6 +173,17 @@ const UserManagement = (props) => {
                     </TableCell>
                     <TableCell>
                       <Typography variant="body1">{curator.email}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      {curator.pw_temp ? (
+                        <IconButton>
+                          <ContentCopyIcon onClick={() => {
+                            navigator.clipboard.writeText(curator.pw_temp);
+                          }} />
+                        </IconButton>
+                      ) : (
+                        <Typography variant="body1" sx={{opacity: 0.5}}>Set by user</Typography>
+                      )}
                     </TableCell>
                     <TableCell>
                       <Typography variant="body1">{curator.Courses.length}</Typography>
@@ -171,19 +205,6 @@ const UserManagement = (props) => {
                           handleChangeUserActivationStatus(e.target.parentElement.attributes.itemid.value, e.target.checked)
                         }}
                       />
-                      {/* <Stack direction="row">
-                        <IconButton
-                          color="primary"
-                          variant="contained"
-                          size="small"
-                          disabled={curator.is_admin}
-                          onClick={() =>
-                            handleDeleteClick(curator.id)
-                          }
-                        >
-                          <DeleteIcon />
-                        </IconButton>
-                      </Stack> */}
                     </TableCell>
                   </TableRow>
                 ))}
@@ -235,7 +256,7 @@ const UserManagement = (props) => {
           </Button>
         </DialogActions>
       </Dialog>
-    </div>
+    </>
   );
 }
 
