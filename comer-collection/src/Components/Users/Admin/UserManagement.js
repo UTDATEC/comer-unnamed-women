e02afae5f0b179e4c35d1f8ed87eb55bb12121b2
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-18 16:48:29 -0600

    Begin adding filter functionality for columns
    This commit only includes the filter option for the UserType column

--- comer-collection/src/Components/Users/Admin/UserManagement.js
+++ comer-collection/src/Components/Users/Admin/UserManagement.js
@@ -9,18 +9,23 @@ import {
   DialogActions,
   Button,
   Typography,
-  Switch, IconButton, Alert
+  Switch, IconButton, Alert, useTheme, Menu, MenuItem, Divider
 } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import AddIcon from "@mui/icons-material/Add";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import Unauthorized from "../../ErrorPages/Unauthorized";
 import SearchBox from "../Tools/SearchBox";
 import Snackbar from "@mui/material/Snackbar";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+import FilterAltOffOutlinedIcon from "@mui/icons-material/FilterAltOffOutlined";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import CheckIcon from "@mui/icons-material/Check";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+
 
 const UserManagement = (props) => {
   const [curators, setCurators] = useState([]);
@@ -33,20 +38,20 @@ const UserManagement = (props) => {
   const [snackbarText, setSnackbarText] = useState("");
   const [snackbarSeverity, setSnackbarSeverity] = useState("success");
 
+  const [userTypeFilter, setUserTypeFilter] = useState(null);
+  const [userTypeMenuAnchorElement, setUserTypeMenuAnchorElement] = useState(null);
+  const handleUserTypeMenuOpen = (event) => {
+    setUserTypeMenuAnchorElement(event.currentTarget);
+  }
+  const handleUserTypeMenuClose = (event) => {
+    setUserTypeMenuAnchorElement(null);
+  }
+
+
   const { user, setUser, selectedNavItem, setSelectedNavItem } = props;
+  const theme = useTheme();
   
 
-  const curatorColumns = {
-    id: "ID",
-    displayName: "Name",
-    email: "Email",
-    password: "Password",
-    courseCount: "Courses",
-    exhibitionCount: "Exhibitions",
-    type: "User Type",
-    isActive: "Active"
-  };
-  
   useEffect(() => {
     setSelectedNavItem("User Management");
     if(user.is_admin) {
@@ -68,6 +73,17 @@ const UserManagement = (props) => {
       console.error("Error fetching data:", error);
     }
   };
+
+  const curatorsToDisplay = curators.filter((curator) => {
+    return !userTypeFilter || userTypeFilter == "Administrator" && curator.is_admin || userTypeFilter == "Curator" && !curator.is_admin
+  }).filter((curator) => {
+    return searchQuery == "" ||
+    Boolean((curator.family_name ?? "").toLowerCase().includes(searchQuery.toLowerCase())) ||
+    Boolean((curator.given_name ?? "").toLowerCase().includes(searchQuery.toLowerCase())) ||
+    Boolean(`${(curator.given_name ?? "").toLowerCase()} ${(curator.family_name ?? "").toLowerCase()}`.includes(searchQuery.toLowerCase())) ||
+    Boolean(`${(curator.family_name ?? "").toLowerCase()}, ${(curator.given_name ?? "").toLowerCase()}`.includes(searchQuery.toLowerCase())) ||
+    Boolean(curator.email?.replace("@utdallas.edu", "").toLowerCase().includes(searchQuery.toLowerCase()))
+  })
   
 
   const handleDeleteClick = (curatorId) => {
@@ -138,42 +154,114 @@ const UserManagement = (props) => {
     <>
         <Stack direction="row" justifyContent="space-between" spacing={2} padding={2}>
           <SearchBox searchQuery={searchQuery} setSearchQuery={setSearchQuery} width="50%" />
-          <Button variant="contained" startIcon={<AddIcon/>} disabled>
-            <Typography variant="body1">Create User</Typography>
-          </Button>
+          <Stack direction="row" spacing={2}>
+            <Button variant="outlined" startIcon={<FilterAltOffOutlinedIcon/>} onClick={() => {
+              setSearchQuery("");
+              setUserTypeFilter(null);
+            }}
+              disabled={
+                !Boolean(searchQuery || userTypeFilter)
+              }>
+              <Typography variant="body1">Clear Filters</Typography>
+            </Button>
+            <Button variant="contained" startIcon={<GroupAddIcon/>}>
+              <Typography variant="body1">Create Users</Typography>
+            </Button>
+          </Stack>
         </Stack>
         <TableContainer component={Paper} sx={{ width: "100%", maxHeight: 'calc(100% - 100px)' }}>
           <Table stickyHeader size="small" aria-label="curator table" sx={{ width: "100%" }}>
             <TableHead>
               <TableRow sx={{backgroundColor: "#CCC"}}>
-                {Object.keys(curatorColumns).map((col) => (
-                  <TableCell key={col} sx={{backgroundColor: "#CCC"}}>
-                    <Typography variant="h6">{curatorColumns[col]}</Typography>
-                  </TableCell>
-                ))}
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">ID</Typography>
+                </TableCell>
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">Name</Typography>
+                </TableCell>
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">Email</Typography>
+                </TableCell>
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">Password</Typography>
+                </TableCell>
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">Courses</Typography>
+                </TableCell>
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">Exhibitions</Typography>
+                </TableCell>
+                <TableCell sx={{backgroundColor: userTypeFilter ? theme.palette.secondary["200"] : "#CCC"}}>
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    <Typography variant="h6">User Type</Typography>
+                    <IconButton onClick={handleUserTypeMenuOpen}>
+                      {
+                        userTypeFilter ? (<FilterAltIcon fontSize="large" color="secondary" />) : (<FilterAltOutlinedIcon fontSize="large" />)
+                      }
+                    </IconButton>
+                  </Stack>
+                  <Menu MenuListProps={{
+                    }} anchorEl={userTypeMenuAnchorElement} anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'center'
+                    }} transformOrigin={{
+                      vertical: "top",
+                      horizontal: "center"
+                    }} open={Boolean(userTypeMenuAnchorElement)} onClose={handleUserTypeMenuClose}>
+                    <MenuItem onClick={() => {
+                      handleUserTypeMenuClose();
+                      setUserTypeFilter(null)
+                    }}>
+                      <Stack direction="row" spacing={1}>
+                        <CheckIcon sx={{ visibility: !userTypeFilter ? "" : "hidden" }}/>
+                        <Typography variant="body">
+                          All Users
+                        </Typography>
+                      </Stack>
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={() => {
+                      handleUserTypeMenuClose();
+                      setUserTypeFilter("Administrator")
+                    }}>
+                      <Stack direction="row" spacing={1}>
+                        <CheckIcon sx={{ visibility: userTypeFilter == "Administrator" ? "" : "hidden" }}/>
+                        <Typography variant="body">
+                          Administrators
+                        </Typography>
+                      </Stack>
+                    </MenuItem>
+                    <MenuItem onClick={() => {
+                      handleUserTypeMenuClose();
+                      setUserTypeFilter("Curator")
+                    }}>
+                      <Stack direction="row" spacing={1}>
+                        <CheckIcon sx={{ visibility: userTypeFilter == "Curator" ? "" : "hidden" }}/>
+                        <Typography variant="body">
+                          Curators
+                        </Typography>
+                      </Stack>
+                    </MenuItem>
+                  </Menu>
+                </TableCell>
+                <TableCell sx={{backgroundColor: "#CCC"}}>
+                  <Typography variant="h6">Active</Typography>
+                </TableCell>
               </TableRow>
             </TableHead>
 
             <TableBody>
-              {curators.map((curator) => (
+              {curatorsToDisplay.map((curator) => (
                   <TableRow key={curator.id} sx={{
                     [`&:hover`]: {
                       backgroundColor: "#EEE"
-                    },
-                    display: (
-                      searchQuery == "" ||
-                      Boolean(curator.family_name?.toLowerCase().includes(searchQuery.toLowerCase())) ||
-                      Boolean(curator.given_name?.toLowerCase().includes(searchQuery.toLowerCase())) ||
-                      Boolean(`${curator.given_name?.toLowerCase()} ${curator.family_name?.toLowerCase()}`.includes(searchQuery.toLowerCase())) ||
-                      Boolean(`${curator.family_name?.toLowerCase()}, ${curator.given_name?.toLowerCase()}`.includes(searchQuery.toLowerCase())) ||
-                      Boolean(curator.email?.replace("@utdallas.edu", "").toLowerCase().includes(searchQuery.toLowerCase()))
-                    ) ? "" : "none"
+                    }
                   }}>
                     <TableCell>
                       <Typography variant="body1">{curator.id}</Typography>
                     </TableCell>
                     <TableCell>
-                      <Typography variant="body1">{curator.family_name}, {curator.given_name}</Typography>
+                      <Typography variant="body1">{curator.family_name || curator.given_name ? `${curator.family_name}, ${curator.given_name}` : ""}</Typography>
                     </TableCell>
                     <TableCell>
                       <Typography variant="body1">{curator.email}</Typography>
