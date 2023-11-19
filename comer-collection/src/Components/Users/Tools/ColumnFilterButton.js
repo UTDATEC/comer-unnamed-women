a5810f0fcc5c52f7151f37a3fd59c9ef6f567a86
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-18 18:26:06 -0600

    Update ColumnFilterButton.js
    Add key prop to each menu item to avoid error message in console

--- comer-collection/src/Components/Users/Tools/ColumnFilterButton.js
+++ comer-collection/src/Components/Users/Tools/ColumnFilterButton.js
@@ -35,7 +35,7 @@ export const ColumnFilterButton = (props) => {
         </MenuItem>
         <Divider />
         {options.map((option) => (
-          <MenuItem onClick={() => {
+          <MenuItem key={option.value} onClick={() => {
             setMenuAnchorElement(null);
             setFilter(option.value);
           }}>
