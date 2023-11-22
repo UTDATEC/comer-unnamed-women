c07f3f78991089e897016d889d666b550a9c5517
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-22 17:10:33 -0600

    Require permanent password for Profile page and add My Courses table

--- comer-collection/src/Components/Users/Profile.js
+++ comer-collection/src/Components/Users/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Card,
@@ -6,19 +6,122 @@ import {
   Typography,
   Button,
   Stack,
+  Box,
+  TableCell,
 } from "@mui/material";
-import { useNavigate } from "react-router";
+import { Navigate, useNavigate } from "react-router";
+import axios from "axios";
+import { DataTable } from "./Tools/DataTable";
 
 const Profile = (props) => {
 
   const { appUser, setAppUser, selectedNavItem, setSelectedNavItem } = props;
   const navigate = useNavigate();
 
+  const [myCourses, setMyCourses] = useState([]);
+  const fetchMyCourses = async() => {
+    try {
+      const response = await axios.get(`http://localhost:9000/api/account/courses`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        }
+      });
+      setMyCourses(response.data.data);
+    } catch(e) {
+      console.log(`Error fetching courses: ${e.message}`);
+    }
+  }
+
   useEffect(() => {
     setSelectedNavItem("Profile");
+    fetchMyCourses();
   }, [])
 
-  return (
+
+  
+  const courseTableFields = [
+    {
+      columnDescription: "Name",
+      generateTableHeaderCell: () => (
+        <TableCell sx={{backgroundColor: "#CCC"}}>
+          <Typography variant="h6">Name</Typography>
+        </TableCell>
+      ),
+      generateTableCell: (course) => (
+        <TableCell>
+          <Typography variant="body1">{course.name}</Typography>
+        </TableCell>
+      )
+    },
+    {
+      columnDescription: "Start",
+      generateTableHeaderCell: () => (
+        <TableCell sx={{backgroundColor: "#CCC"}}>
+          <Typography variant="h6">Start</Typography>
+        </TableCell>
+      ),
+      generateTableCell: (course) => (
+        <TableCell>
+          <Typography variant="body1">{new Date (course.date_start).toLocaleString()}</Typography>
+        </TableCell>
+      )
+    },
+    {
+      columnDescription: "End",
+      generateTableHeaderCell: () => (
+        <TableCell sx={{backgroundColor: "#CCC"}}>
+          <Typography variant="h6">End</Typography>
+        </TableCell>
+      ),
+      generateTableCell: (course) => (
+        <TableCell>
+          <Typography variant="body1">{new Date (course.date_end).toLocaleString()}</Typography>
+        </TableCell>
+      )
+    },
+    {
+      columnDescription: "Status",
+      generateTableHeaderCell: () => (
+        <TableCell sx={{backgroundColor: "#CCC"}}>
+          <Typography variant="h6">Status</Typography>
+        </TableCell>
+      ),
+      generateTableCell: (course) => (
+        <TableCell>
+          {
+            new Date(course.date_start).getTime() > new Date().getTime() && (
+              <Typography variant="body1">Upcoming</Typography>
+            ) || 
+            new Date(course.date_end).getTime() < new Date().getTime() && (
+              <Typography variant="body1">Expired</Typography>
+            ) || 
+            new Date(course.date_end).getTime() >= new Date().getTime() && new Date(course.date_start).getTime() <= new Date().getTime() && (
+              <Typography variant="body1">Active</Typography>
+            )
+          }
+        </TableCell>
+      )
+    },
+    {
+      columnDescription: "Notes",
+      generateTableHeaderCell: () => (
+        <TableCell sx={{backgroundColor: "#CCC"}}>
+          <Typography variant="h6">Notes</Typography>
+        </TableCell>
+      ),
+      generateTableCell: (course) => (
+        <TableCell>
+          <Typography variant="body1">{course.notes}</Typography>
+        </TableCell>
+      )
+    }
+  ]
+
+
+  return appUser.password_change_required && (
+    <Navigate to="/Account/ChangePassword" />
+  ) || !appUser.password_change_required && (
+    <>
     <Container style={{ paddingTop: "30px" }} maxWidth="xs">
       <Card style={{ border: "1px solid lightgrey" }}>
         <CardContent>
@@ -58,6 +161,14 @@ const Profile = (props) => {
         </CardContent>
       </Card>
     </Container>
+    <Stack spacing={2} margin={2}>
+      <Typography variant="h4">My Courses</Typography>
+      <DataTable
+        items={myCourses}
+        tableFields={courseTableFields}
+      />
+    </Stack>
+    </>
   );
 }
 
