a10ea28470d5f668f43bd12b9fbbc83ce8b6ecf3
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-21 23:35:55 -0600

    Experiment with full screen image viewer

---
+++ comer-collection/src/Components/Users/Tools/ImageFullScreenViewer.js
@@ -0,0 +1,20 @@
+import { useTheme } from "@emotion/react"
+import { Backdrop, Box, Stack } from "@mui/material"
+
+export const ImageFullScreenViewer = ({ image, backdropOpen, setBackdropOpen }) => {
+    const theme = useTheme();
+    return (
+        <Backdrop open={backdropOpen} sx={{opacity: 1, zIndex: (theme) => theme.zIndex.drawer + 1}} onClick={() => {
+            setBackdropOpen(false);
+        }}>
+            <Stack direction="row">
+                <Box>
+                    <img src={image?.url} />
+                </Box>
+                <Box>
+                    Information
+                </Box>
+            </Stack>
+        </Backdrop>
+    )
+}
