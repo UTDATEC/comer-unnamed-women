15348403907243bb43e61cd7c762690738ca50e3
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-21 23:34:39 -0600

    Add rowSelectionEnabled property to DataTable

--- comer-collection/src/Components/Users/Tools/DataTable.js
+++ comer-collection/src/Components/Users/Tools/DataTable.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Paper, TableContainer } from "@mui/material";
+import { Checkbox, Paper, TableCell, TableContainer, Typography } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
-export const DataTable = ({ tableFields, items, extraProperties }) => {
+export const DataTable = ({ tableFields, items, extraProperties, rowSelectionEnabled }) => {
   return (
     <TableContainer component={Paper} sx={{ width: "100%", maxHeight: 'calc(100% - 0px)' }}>
       <Table stickyHeader size="small" sx={{ width: "100%" }}>
         <TableHead>
           <TableRow>
+            {Boolean(rowSelectionEnabled) && (<TableCell sx={{backgroundColor: "#CCC"}}>
+              <Typography variant="body1">&nbsp;</Typography>
+            </TableCell>)}
             {tableFields.map((tf) => {
               return (
                 <React.Fragment key={tf.columnDescription}>
@@ -28,6 +31,9 @@ export const DataTable = ({ tableFields, items, extraProperties }) => {
                 backgroundColor: "#EEE"
               }
             }}>
+            {Boolean(rowSelectionEnabled) && (<TableCell width="10px">
+              <Checkbox size="large" />
+            </TableCell>)}
               {tableFields.map((tf) => {
                 return (
                   <React.Fragment key={tf.columnDescription}>
