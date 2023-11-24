db76e9f0e9c7b06141044fe3f5152036707df5fd
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-23 23:25:23 -0600

    Fix null reference error

--- comer-collection/src/Components/Users/Tools/SearchUtilities.js
+++ comer-collection/src/Components/Users/Tools/SearchUtilities.js
@@ -1,6 +1,6 @@
 export const searchItems = (searchQuery, itemsToSearch, fieldsToSearch) => {
   return itemsToSearch.filter((item) => {
-    if(fieldsToSearch.length == 0)
+    if(fieldsToSearch?.length == 0)
       return true;
     for (let f of fieldsToSearch) {
       if (Boolean((item[f] ?? "").toLowerCase().includes(searchQuery.toLowerCase()))) {
