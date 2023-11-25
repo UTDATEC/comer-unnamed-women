7c81bdf92197c5d159594ed5d8625e0e596936a5
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-25 12:29:07 -0600

    Update exhibitionEditReducer.js

--- comer-collection/src/Components/ExhibitionPage/exhibitionEditReducer.js
+++ comer-collection/src/Components/ExhibitionPage/exhibitionEditReducer.js
@@ -1,3 +1,26 @@
+export const blankExhibitionData = {
+    "main": {
+        "exhibition_name": "Placeholder Exhibition Name",
+        "curator": "Placeholder Curator Name",
+        "date_created": "Placeholder Date Created"
+    },
+    "appearance": {
+        "main_wall_color": "#ffffff",
+        "side_wall_color": "#ffffff",
+        "floor_color": "#ffffff",
+        "ceiling_color": "#ffffff",
+        "floor_texture": "parquet_wood.jpg",
+        "moodiness": "moody dark",
+        "ambient_light_color": "#ffffff"
+    },
+    "size": {
+        "length_ft": 25,
+        "width_ft": 25,
+        "height_ft": 10
+    },
+};
+
+
 export const exhibitionEditReducer = (exhibitionData, action) => {
     switch (action.type) {
         case "set_main_wall_color":
@@ -28,25 +51,4 @@ export const exhibitionEditReducer = (exhibitionData, action) => {
             console.log("Unrecognized exhibition edit action: ", action);
             return exhibitionData;
     }
-};export const blankExhibitionData = {
-    "main": {
-        "exhibition_name": "Placeholder Exhibition Name",
-        "curator": "Placeholder Curator Name",
-        "date_created": "Placeholder Date Created"
-    },
-    "appearance": {
-        "main_wall_color": "#ffffff",
-        "side_wall_color": "#ffffff",
-        "floor_color": "#ffffff",
-        "ceiling_color": "#ffffff",
-        "floor_texture": "parquet_wood.jpg",
-        "moodiness": "moody dark",
-        "ambient_light_color": "#ffffff"
-    },
-    "size": {
-        "length_ft": 25,
-        "width_ft": 25,
-        "height_ft": 10
-    },
 };
-
