93f8f77dc57109a23c02e3646728a003f616eac1
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-24 20:19:26 -0600

    Add safe_display_name virtual field for tags

--- comer-collection/api/models/tag.js
+++ comer-collection/api/models/tag.js
@@ -1,3 +1,5 @@
+const { DataTypes } = require("sequelize");
+
 module.exports = (db) => {
     const { sequelize, Sequelize } = db;
     const Tag = sequelize.define("Tag", {
@@ -16,6 +18,12 @@ module.exports = (db) => {
         notes: {
             type: Sequelize.TEXT('tiny'),
             field: "tag_notes"
+        },
+        safe_display_name: {
+            type: DataTypes.VIRTUAL,
+            get() {
+                return (this.data != "" ? this.data : `Tag ${this.id}`)
+            }
         }
     }, {
         tableName: "comer_image_tags"
