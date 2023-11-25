077c9e6f687fc33cf7dd19a5ce974830b413f7ab
vmm20 <57491580+vmm20@users.noreply.github.com>
2023-11-24 22:53:08 -0600

    Consolidate axios requests in one function

--- comer-collection/src/Components/Users/Tools/HelperMethods/APICalls.js
+++ comer-collection/src/Components/Users/Tools/HelperMethods/APICalls.js
@@ -1,20 +1,54 @@
 import axios from "axios";
 import { filterItemFields, userFieldDefinitions } from "./fields";
 
+const apiLocation = "http://localhost:9000";
+
+export const sendAuthenticatedRequest = async(method, url, payload) => {
+  const axiosMethods = {
+    "GET": axios.get,
+    "POST": axios.post,
+    "PUT": axios.put,
+    "DELETE": axios.delete
+  }
+  
+  if(!Object.keys(axiosMethods).includes(method))
+    throw Error(`${method} is not a valid method`);
+
+  switch (method) {
+    case "GET":
+    case "DELETE":
+      const response = await axiosMethods[method](
+        `${apiLocation}${url}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          }
+        }
+      )
+      return response.data;
+  
+    case "POST":
+    case "PUT":
+      const response2 = await axiosMethods[method](
+        `${apiLocation}${url}`, payload, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          }
+        }
+      )
+      return response2.data;
+  }
+
+
+}
+
+
 export const createUsers = async(newUserArray, {showSnackbar, setCreateDialogIsOpen, createDialogDispatch, fetchData}) => {
   let usersCreated = 0;
   let userIndicesWithErrors = []
   for(const [i, newUserData] of newUserArray.entries()) {
     try {
       let filteredUser = filterItemFields(userFieldDefinitions, newUserData)
-      await axios.post(
-        `http://localhost:9000/api/users`, filteredUser,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        }
-      );
+      await sendAuthenticatedRequest("POST", "/api/users", filteredUser)
 
       usersCreated++;
 
@@ -37,13 +71,10 @@ export const createUsers = async(newUserArray, {showSnackbar, setCreateDialogIsO
   } else if(usersCreated < newUserArray.length) {
 
     if(usersCreated > 0) {
-      showSnackbar(`Created ${usersCreated} of ${newUserArray.length} ${newUserArray.length == 1 ? "user" : "users"}.  Make sure the email addresses are unique, and try again.`, "warning");
+      showSnackbar(`Created ${usersCreated} of ${newUserArray.length} ${newUserArray.length == 1 ? "user" : "users"}.  Make sure each user has a unique email address.`, "warning");
     }
     else {
-      showSnackbar(`Failed to create ${newUserArray.length} ${newUserArray.length == 1 ? "user" : "users"}.  
-        ${newUserArray.length == 1 ? 
-          "Make sure the email address is not already in use, and try again." : 
-          "Make sure the email address is not already in use, and try again."}`, "error")
+      showSnackbar(`Failed to create ${newUserArray.length} ${newUserArray.length == 1 ? "user" : "users"}.  Make sure each user has a unique email address.`, "error")
     }
 
     createDialogDispatch({
