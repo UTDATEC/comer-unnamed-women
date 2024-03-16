0a061feeae8e9501a4d12cecf7d36698d7a60263
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-16 00:53:39 -0500

    Create associations.js
    With new manageManyToManyAssociation function

---
+++ comer-collection/api/controllers/associations.js
@@ -0,0 +1,46 @@
+const createError = require('http-errors');
+const { sequelize } = require("../sequelize.js");
+const { Op } = require('sequelize');
+
+
+const manageManyToManyAssociation = async (req, res, next, model, association, action, primaryIds, secondaryIds) => {
+    try {
+        if(!Array.isArray(primaryIds)) {
+            throw "primaryIds must be an array"
+        } else if(!Array.isArray(secondaryIds)) {
+            throw "secondaryIds must be an array"
+        }
+        await sequelize.transaction(async (t) => {
+            const primaries = Array.from(await model.findAll({
+                where: {
+                    id: {
+                        [Op.in]: primaryIds
+                    }
+                },
+                transaction: t
+            }));
+            const { set, addMultiple, removeMultiple } = association.accessors;
+            for(let p of primaries) {
+                switch (action) {
+                    case 'set':
+                        p[set](secondaryIds);
+                        break;
+                    case 'assign':
+                        p[addMultiple](secondaryIds);
+                        break;
+                    case 'unassign':
+                        p[removeMultiple](secondaryIds);
+                        break;
+                    default:
+                        throw "Invalid action for M:N association"
+                }
+            }
+        });
+        res.sendStatus(204);
+    } catch(e) {
+        next(createError(400, { debugMessage: e.message + "\n" + e.stack }));
+    }
+}
+
+
+module.exports = { manageManyToManyAssociation }
