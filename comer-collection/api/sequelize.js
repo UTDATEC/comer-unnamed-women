5d0b6c265a28fda35f2cd253acfab964fec2cb11
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-15 20:58:09 -0500

    Update Sequelize association onDelete behavior

--- comer-collection/api/sequelize.js
+++ comer-collection/api/sequelize.js
@@ -41,21 +41,25 @@ db.User = require("./models/user.js")(db);
 db.Course = require("./models/course.js")(db);
 db.Exhibition = require("./models/exhibition.js")(db);
 
+// Images with artists can be deleted, but artists with images cannot be deleted
+db.Artist.belongsToMany(db.Image, { through: "comer_image_credits", foreignKey: "artist_id", onDelete: 'RESTRICT' });
+db.Image.belongsToMany(db.Artist, { through: "comer_image_credits", foreignKey: "image_id", onDelete: 'CASCADE' });
 
-db.Artist.belongsToMany(db.Image, { through: "comer_image_credits", foreignKey: "artist_id" });
-db.Image.belongsToMany(db.Artist, { through: "comer_image_credits", foreignKey: "image_id" });
+// Images with tags can be deleted, but tags with images cannot be deleted
+db.Tag.belongsToMany(db.Image, { through: "comer_image_tag_assignments", foreignKey: "tag_id", onDelete: 'RESTRICT' });
+db.Image.belongsToMany(db.Tag, { through: "comer_image_tag_assignments", foreignKey: "image_id", onDelete: 'CASCADE' });
 
-db.Tag.belongsToMany(db.Image, { through: "comer_image_tag_assignments", foreignKey: "tag_id"});
-db.Image.belongsToMany(db.Tag, { through: "comer_image_tag_assignments", foreignKey: "image_id"});
+// Users with courses cannot be deleted, and courses with users cannot be deleted
+db.User.belongsToMany(db.Course, { through: "comer_enrollments", foreignKey: "user_id", onDelete: 'RESTRICT'});
+db.Course.belongsToMany(db.User, { through: "comer_enrollments", foreignKey: "course_id", onDelete: 'RESTRICT'});
 
-db.User.belongsToMany(db.Course, { through: "comer_enrollments", foreignKey: "user_id"});
-db.Course.belongsToMany(db.User, { through: "comer_enrollments", foreignKey: "course_id"});
-
-db.User.hasMany(db.Exhibition, {foreignKey: 'exhibition_owner'})
+// Exhibitions can be deleted (with no effect on the owner), but users with exhibitions cannot be deleted
+db.User.hasMany(db.Exhibition, {foreignKey: 'exhibition_owner', onDelete: 'RESTRICT'})
 db.Exhibition.belongsTo(db.User, {foreignKey: 'exhibition_owner', inverse: {as: 'exhibitions', type: 'hasMany'}});
 
-db.Exhibition.belongsToMany(db.Image, { through: "comer_image_appearances", foreignKey: "exhibition_id" });
-db.Image.belongsToMany(db.Exhibition, { through: "comer_image_appearances", foreignKey: "image_id" });
+// Exhibitions with images can be deleted, but images with exhibitions cannot be deleted
+db.Exhibition.belongsToMany(db.Image, { through: "comer_image_appearances", foreignKey: "exhibition_id", onDelete: 'CASCADE' });
+db.Image.belongsToMany(db.Exhibition, { through: "comer_image_appearances", foreignKey: "image_id", onDelete: 'RESTRICT' });
 
 
 module.exports = db;
