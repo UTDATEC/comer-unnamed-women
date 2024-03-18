fafe7d1ef817df396c91f02abcb79c42730eb06d
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-17 22:57:30 -0500

    Run ESLint on AppSnackbar

--- comer-collection/src/Components/App/AppSnackbar.js
+++ comer-collection/src/Components/App/AppSnackbar.js
@@ -1,50 +1,50 @@
 import { Snackbar, Alert, Stack, Typography } from "@mui/material";
-import React, { useCallback, useContext, useMemo, useState } from "react";
-import { createContext } from 'react';
+import React, { useCallback, useContext, useState } from "react";
+import { createContext } from "react";
 
 
 const SnackbarContext = createContext();
 
 export const SnackbarProvider = ({ children }) => {
 
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarText, setSnackbarText] = useState("");
-  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarText, setSnackbarText] = useState("");
+    const [snackbarSeverity, setSnackbarSeverity] = useState("success");
 
-  const showSnackbar = useCallback((message, severity = "info") => {
-    console.log("called showSnackbar");
-    setSnackbarText(message);
-    setSnackbarSeverity(severity);
-    setSnackbarOpen(true);
-  }, [setSnackbarOpen, setSnackbarSeverity, setSnackbarText]);
+    const showSnackbar = useCallback((message, severity = "info") => {
+        console.log("called showSnackbar");
+        setSnackbarText(message);
+        setSnackbarSeverity(severity);
+        setSnackbarOpen(true);
+    }, [setSnackbarOpen, setSnackbarSeverity, setSnackbarText]);
 
-  return (
-    <SnackbarContext.Provider value={showSnackbar}>
-      {children}
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={3000}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center"
-        }}
-        onClose={() => {
-          setSnackbarOpen(false);
-        } }
-      >
-        <Alert severity={snackbarSeverity} variant="standard" sx={{ width: "100%" }}>
-          <Stack direction="row" spacing={2}>
-            <Typography variant="body1">{snackbarText}</Typography>
-          </Stack>
-        </Alert>
-      </Snackbar>
-    </SnackbarContext.Provider>
-  );
+    return (
+        <SnackbarContext.Provider value={showSnackbar}>
+            {children}
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center"
+                }}
+                onClose={() => {
+                    setSnackbarOpen(false);
+                } }
+            >
+                <Alert severity={snackbarSeverity} variant="standard" sx={{ width: "100%" }}>
+                    <Stack direction="row" spacing={2}>
+                        <Typography variant="body1">{snackbarText}</Typography>
+                    </Stack>
+                </Alert>
+            </Snackbar>
+        </SnackbarContext.Provider>
+    );
 };
 
 
 export const useSnackbar = () => {
-  const showSnackbar = useContext(SnackbarContext);
-  return showSnackbar;
+    const showSnackbar = useContext(SnackbarContext);
+    return showSnackbar;
 };
 
