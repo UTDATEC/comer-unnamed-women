490938650ec1ea1a64ea868a260989cfeeba8cff
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-17 22:57:41 -0500

    Run ESLint on AppUser.js

--- comer-collection/src/Components/App/AppUser.js
+++ comer-collection/src/Components/App/AppUser.js
@@ -1,51 +1,51 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 export const AppUserContext = createContext();
 
 export const AppUserProvider = ({ children }) => {
 
   
-  const [appUser, setAppUser] = useState(null);
-  const [appUserIsLoaded, setAppUserIsLoaded] = useState(false);
+    const [appUser, setAppUser] = useState(null);
+    const [appUserIsLoaded, setAppUserIsLoaded] = useState(false);
 
-  const initializeAppUser = async() => {
-    try {
-      if(!localStorage.getItem('token')) {
-        throw "No user is logged in"
-      }
-      const response = await fetch(`${process.env.REACT_APP_API_HOST}/api/user/profile`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
+    const initializeAppUser = async() => {
+        try {
+            if(!localStorage.getItem("token")) {
+                throw "No user is logged in";
+            }
+            const response = await fetch(`${process.env.REACT_APP_API_HOST}/api/user/profile`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            });
+            if(response.status == 200) {
+                let responseJson = await response.json();
+                setAppUser(responseJson.data);
+            } else {
+                throw new Error("Response status was not 200");
+            }
+        } catch (error) {
+            setAppUser(null);
         }
-      })
-      if(response.status == 200) {
-        let responseJson = await response.json();
-        setAppUser(responseJson.data);
-      } else {
-        throw new Error("Response status was not 200")
-      }
-    } catch (error) {
-      setAppUser(null);
-    }
     
-  }
+    };
 
   
-  useEffect(() => {
-    initializeAppUser().then(() => {
-      setAppUserIsLoaded(true);
-    });
-  }, []);
+    useEffect(() => {
+        initializeAppUser().then(() => {
+            setAppUserIsLoaded(true);
+        });
+    }, []);
 
   
-  return (
-    <AppUserContext.Provider value={{ appUser, setAppUser, initializeAppUser, appUserIsLoaded }}>
-      {children}
-    </AppUserContext.Provider>
-  );
+    return (
+        <AppUserContext.Provider value={{ appUser, setAppUser, initializeAppUser, appUserIsLoaded }}>
+            {children}
+        </AppUserContext.Provider>
+    );
 };
 
 export const useAppUser = () => {
-  const { appUser, setAppUser, initializeAppUser, appUserIsLoaded } = useContext(AppUserContext);
-  return [appUser, setAppUser, initializeAppUser, appUserIsLoaded];
+    const { appUser, setAppUser, initializeAppUser, appUserIsLoaded } = useContext(AppUserContext);
+    return [appUser, setAppUser, initializeAppUser, appUserIsLoaded];
 };
