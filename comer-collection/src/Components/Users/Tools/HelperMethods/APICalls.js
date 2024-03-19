3a672dfb25e41fcdb9003748865612315ceb49c6
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-18 23:51:49 -0500

    Adjust options sent with API calls

--- comer-collection/src/Components/Users/Tools/HelperMethods/APICalls.js
+++ comer-collection/src/Components/Users/Tools/HelperMethods/APICalls.js
@@ -11,6 +11,12 @@ export const sendAuthenticatedRequest = async (method, url, payload) => {
         "DELETE": axios.delete
     };
 
+    const options = localStorage.getItem("token") ? {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+    } : {};
+
     try {
         if (!Object.keys(axiosMethods).includes(method))
             throw Error(`${method} is not a valid method`);
@@ -19,19 +25,15 @@ export const sendAuthenticatedRequest = async (method, url, payload) => {
         case "GET":
         case "DELETE":
             if (Array.isArray(url)) {
-                const responses = await Promise.all(url.map((endpoint) => axiosMethods[method](`${apiLocation}${endpoint}`, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("token")}`
-                    }
-                })));
+                const responses = await Promise.all(url.map((endpoint) => axiosMethods[method](`${apiLocation}${endpoint}`, options)));
                 return responses;
             } else {
                 const response = await axiosMethods[method](
-                    `${apiLocation}${url}`, {
+                    `${apiLocation}${url}`, localStorage.getItem("token") ? {
                         headers: {
                             Authorization: `Bearer ${localStorage.getItem("token")}`,
                         }
-                    }
+                    } : {}
                 );
                 return { ...response.data, status: response.status };
             }
@@ -39,19 +41,11 @@ export const sendAuthenticatedRequest = async (method, url, payload) => {
         case "POST":
         case "PUT":
             if (Array.isArray(url)) {
-                const responses = await Promise.all(url.map((endpoint) => axiosMethods[method](`${apiLocation}${endpoint}`, payload, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("token")}`
-                    }
-                })));
+                const responses = await Promise.all(url.map((endpoint) => axiosMethods[method](`${apiLocation}${endpoint}`, payload, options)));
                 return responses;
             } else {
                 const response = await axiosMethods[method](
-                    `${apiLocation}${url}`, payload, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem("token")}`,
-                        }
-                    }
+                    `${apiLocation}${url}`, payload, options
                 );
                 return { ...response.data, status: response.status };
             }
