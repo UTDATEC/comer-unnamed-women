d83f67fb56489735857783a5b036703825a125ca
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-25 02:38:43 -0500

    Rearrange API routes to more hierarchical setup

--- comer-collection/api/app.js
+++ comer-collection/api/app.js
@@ -10,16 +10,8 @@ import helmet, { contentSecurityPolicy, frameguard } from "helmet";
 import hpp from "hpp";
 import toobusy from "toobusy-js";
 import { rateLimit } from "express-rate-limit";
-import jwt from "jsonwebtoken";
 
-
-import apiRouterPublic from "./router_public.js";
-import apiRouterUserTempPw from "./router_user_temp_pw.js";
-import apiRouterUser from "./router_user.js";
-import apiRouterAdmin from "./router_admin.js";
-
-import db from "./sequelize.js";
-const { User, Course, Exhibition } = db;
+import apiRouterMain from "./router_main.js";
 
 var app = express();
 
@@ -70,65 +62,9 @@ app.use(frameguard({
 
 
 
-const requireAuthenticatedUser = async (req, res, next) => {
-    try {
-        const header = req.get("Authorization");
-        if (header == undefined || header == null)
-            throw new Error("Authorization header not present");
-        else if (!header.startsWith("Bearer "))
-            throw new Error("Authorization header does not start with Bearer");
-
-        const token = header.replace("Bearer ", "");
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        const user = await User.findByPk(decoded.id, {
-            include: [Course, Exhibition]
-        });
-
-        if (!user)
-            throw new Error("User not found");
-        else if (!user.is_active)
-            throw new Error("User is not active");
-
-        // Check if password has changed since token was generated
-        else if (`"${decoded.pw_updated}"` !== JSON.stringify(user.pw_updated))
-            throw new Error("Token password update time does not match the latest password update time");
-
-        req.app_user = user;
-        console.log("authorized successfully and User instance attached to request object");
-        next();
-
-    } catch (e) {
-        next(createError(401, { debugMessage: e.message }));
-    }
-};
-
-const requireAdmin = async (req, res, next) => {
-    const { app_user } = req;
-    if (app_user?.is_admin) {
-        next();
-    } else {
-        next(createError(403, { debugMessage: "Action requires admin privileges" }));
-    }
-};
-
-
-const requirePermanentPassword = async (req, res, next) => {
-    const { app_user } = req;
-    if (app_user && !app_user.pw_change_required) {
-        next();
-    } else {
-        next(createError(401, { debugMessage: "Please change your password and try again" }));
-    }
-};
-
 
 // Routes for querying data
-app.use("/api/public", apiRouterPublic);
-app.use("/api/user", requireAuthenticatedUser, apiRouterUserTempPw);
-app.use("/api/user", requirePermanentPassword, apiRouterUser);
-app.use("/api/admin", requireAuthenticatedUser, requirePermanentPassword, requireAdmin, apiRouterAdmin);
-
+app.use("/api", apiRouterMain);
 
 
 // catch 404 and forward to error handler
---
+++ comer-collection/api/router_main.js
@@ -0,0 +1,77 @@
+import { Router } from "express";
+const router = Router();
+import createError from "http-errors";
+import jwt from "jsonwebtoken";
+
+
+import apiRouterPublic from "./router_public.js";
+import apiRouterUserTempPw from "./router_user_temp_pw.js";
+import apiRouterUser from "./router_user.js";
+import apiRouterAdmin from "./router_admin.js";
+
+import db from "./sequelize.js";
+const { User, Course, Exhibition } = db;
+
+
+const requireAuthenticatedUser = async (req, res, next) => {
+    try {
+        const header = req.get("Authorization");
+        if (header == undefined || header == null)
+            throw new Error("Authorization header not present");
+        else if (!header.startsWith("Bearer "))
+            throw new Error("Authorization header does not start with Bearer");
+
+        const token = header.replace("Bearer ", "");
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findByPk(decoded.id, {
+            include: [Course, Exhibition]
+        });
+
+        if (!user)
+            throw new Error("User not found");
+        else if (!user.is_active)
+            throw new Error("User is not active");
+
+        // Check if password has changed since token was generated
+        else if (`"${decoded.pw_updated}"` !== JSON.stringify(user.pw_updated))
+            throw new Error("Token password update time does not match the latest password update time");
+
+        req.app_user = user;
+        console.log("authorized successfully and User instance attached to request object");
+        next();
+
+    } catch (e) {
+        next(createError(401, { debugMessage: e.message }));
+    }
+};
+
+const requireAdmin = async (req, res, next) => {
+    const { app_user } = req;
+    if (app_user?.is_admin) {
+        next();
+    } else {
+        next(createError(403, { debugMessage: "Action requires admin privileges" }));
+    }
+};
+
+
+const requirePermanentPassword = async (req, res, next) => {
+    const { app_user } = req;
+    if (app_user && !app_user.pw_change_required) {
+        next();
+    } else {
+        next(createError(401, { debugMessage: "Please change your password and try again" }));
+    }
+};
+
+
+// Routes for querying data
+router.use("/public", apiRouterPublic);
+router.use("/user", requireAuthenticatedUser, apiRouterUserTempPw);
+router.use("/user", requirePermanentPassword, apiRouterUser);
+router.use("/admin", requireAuthenticatedUser, requirePermanentPassword, requireAdmin, apiRouterAdmin);
+
+
+
+export default router;
