13f7a6264aa9c2f661e38aeb30347cdcf23514bc
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-03-25 15:37:07 -0500

    Remove web-vitals

--- comer-collection/src/index.js
+++ comer-collection/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import App from "./Components/App/App.js";
-import reportWebVitals from "./reportWebVitals.js";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
@@ -11,8 +10,3 @@ root.render(
         <App />
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
